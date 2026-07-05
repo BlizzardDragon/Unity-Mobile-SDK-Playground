ae98b62b6c21aa6c0dfb6ce802e586bdd1e19f12
daniil-pitetsky <bdden@list.ru>
2026-07-05 21:36:20 +0300

    Added test device for Ads

--- Assets/_project/Scripts/Core/SDK/Ads/AdsCore.cs
+++ Assets/_project/Scripts/Core/SDK/Ads/AdsCore.cs
@@ -1,5 +1,6 @@
 using GoogleMobileAds.Api;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _project.Scripts.Core.SDK.Ads
@@ -8,15 +9,25 @@ namespace _project.Scripts.Core.SDK.Ads
     {
         public static void Initialize(Action<InitializationStatus> onComplete)
         {
+            var requestConfig = new RequestConfiguration
+            {
+                TestDeviceIds = new List<string>
+                {
+                    "E459B4FF1D81AC6F930D04373E204480"
+                }
+            };
+            MobileAds.SetRequestConfiguration(requestConfig);
+
+            // Проверка: выводим установленный список
+            Debug.Log($"Test Device IDs set: {string.Join(", ", requestConfig.TestDeviceIds)}");
+
             MobileAds.Initialize(status =>
             {
-                // Переключаемся в главный поток
                 UnityMainThreadDispatcher.Instance.Execute(() =>
                 {
                     Debug.Log("Google Mobile Ads initialized");
                     onComplete?.Invoke(status);
                 });
             });
-        }
-    }
+        }    }
 }
